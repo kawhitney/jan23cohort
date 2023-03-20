f8ce520e32bc4b2c2bcd015d1cf7a54f167321c0
Kaitlynn Whitney <kailiwhitney@hotmail.com>
2023-03-20 10:38:13 -0500

    First Java Program assignment complete

---
+++ assignments/java/FirstJavaProgram/FJP.java
@@ -0,0 +1,9 @@
+package assignments.java.FirstJavaProgram;
+
+public class FJP {
+    public static void main(String[] args){
+        System.out.println("My name is Katie");
+        System.out.println("I am 29 years old.");
+        System.out.println("I am from Grand Junction, CO");
+    }
+}
