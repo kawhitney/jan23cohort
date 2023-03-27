0683bb95d834a8caf992c4f6df828137b0eb0fec
Kaitlynn Whitney <kailiwhitney@hotmail.com>
2023-03-27 07:42:36 -0500

    Completing CareSoft assignment

---
+++ assignments/java/CareSoft/src/test/java/com/codingdojo/caresoft/CareSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.caresoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
