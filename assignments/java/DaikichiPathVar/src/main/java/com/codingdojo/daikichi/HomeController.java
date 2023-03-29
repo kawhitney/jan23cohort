d0eb075b3dd88e813b1f507512b51e6d800d8ea7
Kaitlynn Whitney <kailiwhitney@hotmail.com>
2023-03-29 10:51:56 -0500

    Daikichi Path Var assignment complete

--- assignments/java/DaikichiPathVar/src/main/java/com/codingdojo/daikichi/HomeController.java
+++ assignments/java/DaikichiPathVar/src/main/java/com/codingdojo/daikichi/HomeController.java
@@ -1,5 +1,38 @@
 package com.codingdojo.daikichi;
 
-public class HomeController {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index(Model moel, HttpSession session) {
+		session.setAttribute("phrase", "");
+		return "index.jsp";
+	}
+	
+	// congrats with city name
+	@RequestMapping("daikichi/travel/{city}")
+	public String travel(@PathVariable String city, Model model, HttpSession session) {
+		session.setAttribute("phrase", String.format("Congratulations! You will soon travel to %s", city));
+		return "index.jsp";
+	}
+	
+	// journey in the near future
+	@RequestMapping("daikichi/lotto/{num}")
+	public String journey(@PathVariable int num, Model model, HttpSession session) {
+		if(num % 2 == 0) {
+			session.setAttribute("phrase", "Your will take a grand journey in the near future, but be warty of tempting offers.");
+		}
+		else {
+			session.setAttribute("phrase", "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.");
+		}
+		return "index.jsp";
+	}
+	
 }
+
--- assignments/java/DaikichiPathVar/src/main/webapp/WEB-INF/index.jsp
+++ assignments/java/DaikichiPathVar/src/main/webapp/WEB-INF/index.jsp
@@ -16,7 +16,7 @@
     <script src='/webjars/jquery/jquery.min.js'>    </script>
     <script src='/webjars/bootstrap/js/boostrap.min.js'>    </script>
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <!-- My JS -->
     <script type = "text/javascript" src='/js/scripts.js'></script>
     <meta charset="UTF-8">
@@ -31,7 +31,7 @@
 		</nav>
 	</header>
 	<main>
-		<h1>Accessible URLs:</h1>
+        <c:out value="${phrase}"></c:out>
 	</main>
 	<footer>
 	
